665247740feac8c33125629be1e057141f9af5e2
axxxay <akkiakshay440@gmail.com>
2024-03-13 17:48:38 +0530

    developed agency and collge account manager forms

---
+++ client/src/components/CollegeAgencyForm/AdditionalDetails.js
@@ -0,0 +1,72 @@
+import { Oval } from 'react-loader-spinner';
+
+const AdditionalDetails = (props) => {
+    const { 
+        handlePerson1InputChange,
+        handlePerson2InputChange,
+        additionalDetails,
+        onSubmitAdditionalDetails,
+        handleCurrentStep,
+        loading,
+        error
+    } = props;
+    return(
+        <div className='hr-form-container'>
+            <h1 className='form-title'>Additional Details</h1>
+            <form className='hr-form' onSubmit={onSubmitAdditionalDetails}>
+                <p className='hr-form-subtitle'>( <span className='hr-form-span'>*</span> ) Indicates required field</p>
+
+                <label className='hr-label'>List any two contact persons related to your organization.<span className='hr-form-span'> *</span></label>
+
+                <p className='person-label'>Contact Person 1</p>
+
+                <label htmlFor='name' className='hr-label'>Name<span className='hr-form-span'> *</span></label>
+                <input type='text' placeholder="Ex: John Doe" className='hr-input' id='name' required value={additionalDetails.person1.name} onChange={handlePerson1InputChange} name='name' />
+
+                <label htmlFor='phone-number' className='hr-label'>Contact Number<span className='hr-form-span'> *</span></label>
+                <input type='number' placeholder="Ex: 9876543210" className='hr-input' id='phone-number' required value={additionalDetails.person1.phone} onChange={handlePerson1InputChange} name='phone' />
+
+                <label htmlFor='email' className='hr-label'>Mail ID<span className='hr-form-span'> *</span></label>
+                <input type='email' placeholder="Ex: hr@earlyjobs.in" className='hr-input' id='email' required value={additionalDetails.person1.email} onChange={handlePerson1InputChange} name='email' />
+
+                <p className='person-label'>Contact Person 2</p>
+
+                <label htmlFor='name2' className='hr-label'>Name<span className='hr-form-span'> *</span></label>
+                <input type='text' placeholder="Ex: John Doe" className='hr-input' id='name2' required value={additionalDetails.person2.name} onChange={handlePerson2InputChange} name='name' />
+
+                <label htmlFor='phone-number2' className='hr-label'>Contact Number<span className='hr-form-span'> *</span></label>
+                <input type='number' placeholder="Ex: 9876543210" className='hr-input' id='phone-number2' required value={additionalDetails.person2.phone} onChange={handlePerson2InputChange} name='phone' />
+
+                <label htmlFor='email2' className='hr-label'>Mail ID<span className='hr-form-span'> *</span></label>
+                <input type='email' placeholder="Ex: hr@earlyjobs.in" className='hr-input' id='email2' required value={additionalDetails.person2.email} onChange={handlePerson2InputChange} name='email' />
+
+                <div className='hr-submit-con'>
+                    <button type='button' className='hr-form-btn'  onClick={() => handleCurrentStep(0)}>Back</button>
+                    <button type='submit' className='hr-form-btn' disabled={loading} >
+                        {loading &&
+                            <span className='hr-oval'>
+                                <Oval
+                                    visible={true}
+                                    height="20"
+                                    width="20"
+                                    color="#ffffff"
+                                    strokeWidth="4"
+                                    ariaLabel="oval-loading"
+                                    wrapperStyle={{}}
+                                    secondaryColor="#ffffff"
+                                    wrapperClass=""
+                                    className='hr-oval'
+                                />
+                            </span>
+                        }
+                        Submit
+                    </button>
+                </div>
+
+                <p className='hr-main-error'>{error}</p>
+            </form>
+        </div>
+    )
+}
+
+export default AdditionalDetails;
---
+++ client/src/components/CollegeAgencyForm/CollegeAgency.js
@@ -0,0 +1,111 @@
+import Cookies from 'js-cookie';
+import Select from 'react-select'
+
+const countryOptions = [
+    { value: '+91', label: '+91'},
+    { value: '+1', label: '+1'},
+    { value: '+44', label: '+44'},
+    { value: '+237', label: '+237'},
+];
+
+const defaultSelectedValue = countryOptions[0].value;
+
+const customStyles = {
+    control: (provided, state) => ({
+        ...provided,
+        border: '1px solid #EB6A4D',
+        borderRadius: '5px',
+        borderTopRightRadius: '0px',
+        borderBottomRightRadius: '0px',
+        borderLeft: '1px',
+        boxShadow: null,
+        '&:hover': {
+            borderColor: '#EB6A4D',
+        },
+        width: '70px',
+        height: '35px',
+        minHeight: '35px',
+        fontSize: '14px',
+    }),
+    dropdownIndicator: (provided) => ({
+        ...provided,
+        color: '#EB6A4D',
+        '&:hover': {
+            color: '#EB6A4D',
+        },
+        width: '15px',
+        padding: '0px',
+        margin: '0px',
+        border: '0px',
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        backgroundColor: state.isSelected ? '#EB6A4D' : null,
+        color: state.isSelected ? 'white' : 'black',
+    }),
+};
+
+const CollegeAgency = ({handleInputChange, collegeAgencyDetails, handleCurrentStep, onSubmitOrganisationDetails, error}) => {
+    const userDetailsId = Cookies.get('user_details_id');
+    return (
+        <div className='hiring-partner-container' style={{marginTop: "0px"}}>
+            <div className='hr-form-container'>
+                <h1 className='form-title'>Organisation Details</h1>
+                <form onSubmit={onSubmitOrganisationDetails} className='hr-form'>
+                    <p className='hr-form-subtitle'>( <span className='hr-form-span'>*</span> ) Indicates required field</p>
+
+                    <label htmlFor='orgName' className='hr-label'>{userDetailsId === 'CLG' ? 'College Name' : 'Agency Name'}<span className='hr-form-span'> *</span></label>
+                    <input type='text' placeholder="Organisation Name" disabled={userDetailsId === "CLG" || userDetailsId === "AGY"} onChange={handleInputChange} value={collegeAgencyDetails.orgName} required className='hr-input' id='orgName' name='orgName' />
+
+                    <label htmlFor='officePhone' className='hr-label'>Office Phone Number<span className='hr-form-span'> *</span></label>
+                    <div className="hr-input phone-select">
+                        <Select
+                            options={countryOptions}
+                            defaultValue={{ value: defaultSelectedValue, label: countryOptions[0].label }}
+                            isSearchable={true}
+                            // onChange={handleInputChange}
+                            styles={customStyles}
+                        />
+                        <input type='number' placeholder="Ex: 9876543210" disabled={userDetailsId === "CLG" || userDetailsId === "AGY"} onChange={handleInputChange} value={collegeAgencyDetails.officePhone} required className='hr-input-select' id='officePhone' name='officePhone' />
+                    </div>
+
+                    <label htmlFor='officeEmail' className='hr-label'>Office Email<span className='hr-form-span'> *</span></label>
+                    <input type='email' placeholder="Ex: hr@earlyjobs.in" disabled={userDetailsId === "CLG" || userDetailsId === "AGY"} onChange={handleInputChange} value={collegeAgencyDetails.officeEmail} required className='hr-input' id='officeEmail' name='officeEmail' />
+
+                    {
+                        (userDetailsId === "CLG" || userDetailsId === "AGY") ?
+                        <>
+                            <label htmlFor='password' className='hr-label'>New Password<span className='hr-form-span'> *</span></label>
+                            <input type='password' placeholder="New password" onChange={handleInputChange} value={collegeAgencyDetails.password} required className='hr-input' id='password' name='password' />
+                            
+                            <label htmlFor='confirm-password' className='hr-label'>Confirm Password<span className='hr-form-span'> *</span></label>
+                            <input type='password' placeholder="Confirm password" onChange={handleInputChange} value={collegeAgencyDetails.confirmPassword} required className='hr-input' id='confirm-password' name='confirmPassword' />
+                        </>
+                    : null
+                    }
+                    
+                    <label htmlFor='officeAddress' className='hr-label'>Office Address<span className='hr-form-span'> *</span></label>
+                    <input type='text' placeholder="Address" onChange={handleInputChange} value={collegeAgencyDetails.officeAddress} required className='hr-input' id='officeAddress' name='officeAddress' />
+
+                    {
+                        userDetailsId === "AGY" ?
+                        <>
+                            <label htmlFor='panNum' className='hr-label'>Company Pan Number<span className='hr-form-span'> *</span></label>
+                            <input type='text' placeholder="Ex: AAAAA1111A" onChange={handleInputChange} value={collegeAgencyDetails.panNum} required className='hr-input' id='panNum' name='panNum' />
+                            
+                            <label htmlFor='GSTNum' className='hr-label'>GST Number</label>
+                            <input type='text' placeholder="Enter GST Number" onChange={handleInputChange} value={collegeAgencyDetails.GSTNum} required className='hr-input' id='GSTNum' name='GSTNum' />
+                        </>
+                    : null
+                    }
+
+                    <p className='hr-main-error'>{error}</p>
+
+                    <button type='submit' className='hr-form-btn'>Save & Next</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default CollegeAgency
