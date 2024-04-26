f646c2214ca952f93897a2e8feb4decfca1f31e8
axxxay <akkiakshay440@gmail.com>
2024-04-26 17:23:18 +0530

    migrated offer letter generation code to hrmanager component

--- client/src/components/HiringManagerDetailsForm/index.js
+++ client/src/components/HiringManagerDetailsForm/index.js
@@ -4,12 +4,18 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import {v4 as uuidv4} from 'uuid';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
+import { format, addDays } from 'date-fns';
+import { PDFDocument, StandardFonts } from 'pdf-lib';
+import Cookie from 'js-cookie';
 import emailjs from '@emailjs/browser';
 import IdentityProofForm from '../HiringPartnerForm/IdentityProof';
 import PersonalDetailsForm from '../HiringPartnerForm/PersonalDetails';
 import QualificationForm from '../HiringPartnerForm/QualificationForm';
 import AboutForm from '../HiringPartnerForm/AboutForm';
 import ReferencesForm from '../HiringPartnerForm/ReferencesForm';
+import Victaman_intern_offer_letter from "../../assets/Joining_Letter_for_Internship.pdf"
+import Victaman_Freelance_Contract from "../../assets/Freelance_Recruiter_Contract.pdf"
+
 import app from '../../firebase';
 
 const customStyles = {
@@ -488,15 +494,6 @@ const HiringManagerDetailsForm = () => {
 
     const onSubmitQualification = (e) => {
         e.preventDefault()
-        // if(certification !== "" && qualification.certification.length === 0) {
-        //     setError("*Please enter certification")
-        //     return
-        // } else 
-        // if(workExperience !== "" && qualification.workExperience.length === 0) {
-        //     setError("*Please enter work experience")
-        //     return
-        // }
-        // setError("")
         console.log(qualification)
         handleCurrentStep(2)
     }
@@ -616,6 +613,214 @@ const HiringManagerDetailsForm = () => {
         return await promise;
     };
 
+    const getOfferLetterCount = async () => {
+        const date = format(new Date(), 'yyyy-MM-dd')
+        const url = `${process.env.REACT_APP_BACKEND_API_URL}/admin/offer-letter-count/${date}`
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: 'Bearer ' + Cookie.get('jwt_token')
+            }
+        }
+        const response = await fetch(url, options)
+        const data = await response.json()
+        console.log(data)
+        console.log(Object.keys(data).length === 0)
+        let count = 0;
+        if(response.ok === true) {
+            if(Object.keys(data).length === 0) {
+                count = 0
+            } else {
+                count = data.count.count
+            }
+            return count
+        } else {
+            setError(data.error)
+            return false
+        }
+    }
+
+    const updateOfferLetterCount = async () => {
+        const date = format(new Date(), 'yyyy-MM-dd')
+        const url = `${process.env.REACT_APP_BACKEND_API_URL}/admin/offer-letter-count/update/${date}`
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: 'Bearer ' + Cookie.get('jwt_token')
+            }
+        }
+        const response = await fetch(url, options)
+        const data = await response.json()
+        console.log(data)
+        if(response.ok === true) {
+            if(data.error) {
+                setError(data.error)
+                return false
+            } else {
+                setError("")
+                console.log(data.message)
+                return true
+            }
+        } else {
+            setError(data.error)
+            return false
+        }
+    }
+
+    const uploadPdf = async (pdfFile) => {
+        const result = await updateOfferLetterCount();
+        if(!result) return;
+        const storage = getStorage(app);
+        const storageRef = ref(storage, `HROfferLetters/${personalDetails.fullName}_${Date()}.pdf`);
+        const uploadTask = uploadBytesResumable(storageRef, pdfFile);
+        let pdfURL = '';
+      
+        // Create a new promise to handle the upload task
+        const promise = new Promise((resolve, reject) => {
+          uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+            },
+            (error) => {
+              console.log(error);
+              reject(error);
+            },
+            async () => {
+              const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+              console.log('File available at', downloadURL);
+              pdfURL = downloadURL;
+              resolve(pdfURL);
+            }
+          );
+        });
+      
+        return promise;
+    }; 
+
+    const generatePDF = async () => {
+        const count = await getOfferLetterCount();
+        if(count === false) return;
+        try {
+            // Load the existing PDF
+            let pdfDoc;
+            if(personalDetails.applyFor === "Intern HR Recruiter") {
+                const response = await fetch(Victaman_intern_offer_letter); // Load the PDF bytes using your preferred method
+                const arrayBuffer = await response.arrayBuffer();
+                const existingPdfBytes = new Uint8Array(arrayBuffer);
+        
+                // Load the PDF document
+                pdfDoc = await PDFDocument.load(existingPdfBytes);
+            } else {
+                const response = await fetch(Victaman_Freelance_Contract); // Load the PDF bytes using your preferred method
+                const arrayBuffer = await response.arrayBuffer();
+                const existingPdfBytes = new Uint8Array(arrayBuffer);
+        
+                // Load the PDF document
+                pdfDoc = await PDFDocument.load(existingPdfBytes);
+            }
+      
+            // Modify the PDF content as needed
+            // Example: Add text to the first page
+            const currDate = new Date();
+            const day = currDate.getDate();
+            const month = currDate.getMonth() + 1;
+            const year = currDate.getFullYear();
+            const currDateStr = `${day < 10 ? "0"+day : day}- ${month < 10 ? "0"+month : month}- ${year}`;
+            const dateRefCount = `- Vic/${format(currDate, 'yy/MMM/dd')}/${(count+1).toString()}`;
+            const dateAfter3Days = addDays(currDate, 3);
+            const lastJoiningDate = format(dateAfter3Days, 'MMMM dd, yyyy');
+
+            if(personalDetails.applyFor === "Intern HR Recruiter") {
+                const pdfDate = pdfDoc.getPages()[0];
+                pdfDate.drawText(currDateStr, {
+                    x: 79, 
+                    y: 729,
+                    size: 10,
+                });
+                const pdfRef = pdfDoc.getPages()[0];
+                pdfRef.drawText(dateRefCount, {
+                    x: 470, 
+                    y: 729,
+                    size: 10.5,
+                });
+                const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+                const pdfName = pdfDoc.getPages()[0];
+                pdfName.drawText(personalDetails.fullName, { 
+                    x: 84,
+                    y: 706,
+                    size: 12,
+                    font: timesRomanFont,
+                });
+                const pdfAddress = pdfDoc.getPages()[0];
+                pdfAddress.drawText(personalDetails.currAddress, { 
+                    x: 95, 
+                    y: 692,
+                    size: 12,
+                    font: timesRomanFont,
+                });
+                const pdfJoiningDate = pdfDoc.getPages()[0];
+                pdfJoiningDate.drawText(`${lastJoiningDate}`, { 
+                    x: 499, 
+                    y: 618.5,
+                    size: 10,
+                });
+            } else if (personalDetails.applyFor === "Freelance HR Recruiter") {
+                const pdfDate = pdfDoc.getPages()[0];
+                pdfDate.drawText(currDateStr, {
+                    x: 345, 
+                    y: 681,
+                    size: 12,
+                });
+                const pdfRef = pdfDoc.getPages()[0];
+                pdfRef.drawText("Ref: " +dateRefCount, {
+                    x: 440, 
+                    y: 716,
+                    size: 11,
+                });
+                const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+                const pdfName = pdfDoc.getPages()[0];
+                pdfName.drawText(personalDetails.fullName, { 
+                    x: 44,
+                    y: 632,
+                    size: 14,
+                    font: timesRomanFont,
+                });
+                const pdfAddress = pdfDoc.getPages()[0];
+                pdfAddress.drawText(personalDetails.currAddress, { 
+                    x: 275, 
+                    y: 632,
+                    size: 14,
+                    font: timesRomanFont,
+                });
+            }
+      
+            // Save the modified PDF
+            const modifiedPdfBytes = await pdfDoc.save();
+      
+            // Perform further actions (e.g., send the modified PDF to the server, download, etc.)
+            console.log('PDF Modified:', modifiedPdfBytes);
+            
+            try {
+                const pdfURL = await uploadPdf(modifiedPdfBytes);
+                // setSignUpDetails({ ...signUpDetails, resumeUrl: pdfURL })
+                // return modifiedPdfBytes;
+                return pdfURL;
+            } catch (error) {
+                console.error('Error uploading PDF:', error);
+            }
+
+          } catch (error) {
+            console.error('Error modifying PDF:', error);
+          }
+    }
+
+
     const sendEmail = (formData) => {
         const languages = formData.personalDetails.languages.map((language) => language.value).join(', ')
         // const certification = formData.qualification.certification.map((certification) => certification.value).join(', ')
@@ -662,9 +867,36 @@ const HiringManagerDetailsForm = () => {
         } else {
             setError(data.error)
         }
-        setLoading(false)
     }
     
+    const postOfferLetterToDB = async (pdfURL) => {
+        const offerLetter = {
+            offerLetterUrl: pdfURL,
+        }
+        const email = Cookies.get('email')
+        const url = `${process.env.REACT_APP_BACKEND_API_URL}/api/users/hr-resumes/${email}`
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization: 'Bearer ' + Cookie.get('jwt_token')
+            },
+            body: JSON.stringify(offerLetter)
+        }
+        const response = await fetch(url, options)
+        const data = await response.json()
+        console.log(data)
+        if(response.ok === true) {
+            if(data.error) {
+                setError(data.error)
+            } else {
+                return true;
+            }
+        } else {
+            setError(data.error)
+        }
+    }
 
     const onSubmitToFirestore = async (formData) => {
         console.log(formData)
@@ -676,13 +908,19 @@ const HiringManagerDetailsForm = () => {
 
         console.log(docRef)
         if(docRef) {
-            updateUserDetailsAndPassowrdInDB(personalDetails.email, docId, personalDetails.currAddress, personalDetails.password)
+            await updateUserDetailsAndPassowrdInDB(personalDetails.email, docId, personalDetails.currAddress, personalDetails.password)
+            const role = Cookies.get('role')
+            if(role === "HR") {
+                const pdfURL = await generatePDF()
+                await postOfferLetterToDB(pdfURL);
+            }
         }
+        setLoading(false)
     }
 
     const onSubmitIdentityProof = async (e) => {
         e.preventDefault()
-
+        // return
         console.log('triggered')
         setLoading(true)
 
