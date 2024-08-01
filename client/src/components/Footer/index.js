da9953076369f0e8f42cf1e54a11c96cc4bd0761
axxxay <akkiakshay440@gmail.com>
2024-08-01 13:30:43 +0530

    updated navbar and added new item in category list

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,9 @@
-import { FaFacebook, FaLinkedin, FaTelegram } from "react-icons/fa";
+import { FaFacebook, FaLinkedin, FaTelegram, FaAppStore, FaGooglePlay } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import './style.css';
 import { Link } from "react-router-dom";
 import { SlLocationPin } from "react-icons/sl";
-import { HiOutlinePhone } from "react-icons/hi";
+import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
 
 const Footer = ({handleShowContactForm}) => {
     return (
@@ -11,41 +11,23 @@ const Footer = ({handleShowContactForm}) => {
             <div className="footer-main-container">
                 <div className="footer-address-container">
                     <Link to='/' className="footer-logo-link">
-                        <img src="/early-jobs-logo2.png" alt="earlyjobs" className="footer-logo" />
+                        <img src="/footer-logo.png" alt="earlyjobs" className="footer-logo" />
                     </Link>
-                    <p className="footer-address">© 2024 EarlyJobs | All rights reserved.</p>
-                    <h3 className="footer-heading">OUR OFFICE</h3>
+                    {/* <p className="footer-address">© 2024 EarlyJobs | All rights reserved.</p> */}
+                    {/* <h3 className="footer-heading">OUR OFFICE</h3> */}
                     <div className="footer-sub-con">
                         <SlLocationPin className="footer-icon" />
                         <p className="footer-address">53, HustleHub, 5th Cross Rd, near Sony World Signal, 4th Block, Koramangala, Bengaluru, Karnataka 560034</p>
                     </div>
                     <div className="footer-sub-con">
+                        <HiOutlineMail className="footer-icon" />
+                        <a href="mailto:info@earlyjobs.in" className="footer-address">info@earlyjobs.in</a>
+                    </div>
+                    <div className="footer-sub-con">
                         <HiOutlinePhone className="footer-icon" />
                         <a href="tel:+918217527926" className="footer-address">+91 8217527926</a>
                     </div>
-                </div>
-                <div className="footer-links-container">
-                    <h3 className="footer-heading">COMPANY</h3>
-                    <Link to='/view-openings' className="footer-link">View Openings</Link>
-                    <Link to='/about' className="footer-link">About Us</Link>
-                    <Link to='/team' className="footer-link">Team</Link>
-                    <Link to='/franchise-with-us' className="footer-link">Franchise With Us</Link>
-                    <Link to='/partner-with-us' className="footer-link">Partner With Us</Link>
-                    <Link to='/terms-and-conditions' className="footer-link">Terms & Conditions</Link>
-                    <Link to='/privacy-policy' className="footer-link">Privacy Policy</Link>
-                    <p className="footer-link" rel="noreferrer" id="contact-link" onClick={handleShowContactForm}>Contact Us</p>
-                </div>
-                <div className="footer-links-container">
-                    <h3 className="footer-heading">HIRING TALENT FOR</h3>
-                    <Link to='/view-openings' className="footer-link">Multi National Companies</Link>
-                    <Link to='/view-openings' className="footer-link">Healthcare Companies</Link>
-                    <Link to='/view-openings' className="footer-link">Fintech Companies</Link>
-                    <Link to='/view-openings' className="footer-link">Startups</Link>
-                    <Link to='/view-openings' className="footer-link">Salesforce Need</Link>
-                    <Link to='/view-openings' className="footer-link">BPO Companies</Link>
-                </div>
-                <div className="footer-links-container">
-                    <h3 className="footer-heading">SOCIAL</h3>
+                    {/* <h3 className="footer-heading">SOCIAL</h3> */}
                     <div className="footer-social-media">
                         <a href="https://www.facebook.com/earlyjobs.in" className="footer-social-media-link" rel="noreferrer" target="_blank">
                             <FaFacebook className="footer-social-media-icon" />
@@ -63,15 +45,49 @@ const Footer = ({handleShowContactForm}) => {
                             <FaTelegram className="footer-social-media-icon" />
                         </a>
                     </div>
+                    <h3 className="footer-heading">ALSO AVAILABLE IN</h3>
+                    <div className="footer-sub-con">
+                        <a href="https://play.google.com/store/apps/details?id=com.victaman.earlyjobs" rel="noreferrer" target="_blank" className="footer-store-icons">
+                            <img src="/google-play-badge-logo.svg" draggable={false} alt="google-play" className="footer-store-icon" />
+                            <img src="/app-store-logo.svg" draggable={false} alt="app-store" className="footer-store-icon" />
+                        </a>
+                    </div>
+                </div>
+                <div className="footer-links-container">
+                    <h3 className="footer-heading">COMPANY</h3>
+                    <Link to='/view-openings' className="footer-link">View Openings</Link>
+                    <Link to='/about' className="footer-link">About Us</Link>
+                    <Link to='/team' className="footer-link">Our Team</Link>
+                    <Link to='/franchise-with-us' className="footer-link">Franchise With Us</Link>
+                    <Link to='/partner-with-us' className="footer-link">Partner With Us</Link>
+                    <Link to='/terms-and-conditions' className="footer-link">Terms & Conditions</Link>
+                    <Link to='/privacy-policy' className="footer-link">Privacy Policy</Link>
+                    <p className="footer-link" rel="noreferrer" id="contact-link" onClick={handleShowContactForm}>Contact Us</p>
+                </div>
+                <div className="footer-links-container">
+                    <h3 className="footer-heading">OUR SERVICES</h3>
+                    <Link to='/it-recruitment' className="footer-link">IT Recruitment</Link>
+                    <Link to='/finance-and-accounting-recruitment' className="footer-link">Finance & Accounting Recruitment</Link>
+                    <Link to='/sales-marketing-services' className="footer-link">Sales & Marketing Recruitment</Link>
+                    <Link to='/top-executive-recruitment-firm' className="footer-link">Top Executive Recruitment</Link>
+                    <Link to='/hr-executive-recruitment-services' className="footer-link">HR & Executive Recruitment</Link>
+                    <Link to='/view-openings' className="footer-link">Recruitment Process Outsourcing</Link>
+                    <Link to='/view-openings' className="footer-link">Value Staffing Services</Link>
+                    <Link to='/view-openings' className="footer-link">IT Staffing Services</Link>
+                </div>
+                <div className="footer-links-container">
                     <h3 className="footer-heading">LOCATIONS</h3>
-                    <Link to='/view-openings?page=1&location=Bengaluru' className="footer-link">Bengaluru</Link>
-                    <Link to='/view-openings?page=1&location=Chennai' className="footer-link">Chennai</Link>
-                    <Link to='/view-openings?page=1&location=Pune' className="footer-link">Pune</Link>
-                    <Link to='/view-openings?page=1&location=Mumbai' className="footer-link">Mumbai</Link>
-                    <Link to='/view-openings?page=1&location=Hyderabad' className="footer-link">Hyderabad</Link>
-                    <Link to='/view-openings?page=1&location=New Delhi' className="footer-link">New Delhi</Link>
+                    <Link to='/view-openings?page=1&location=Bengaluru' className="footer-link">Jobs in Bengaluru</Link>
+                    <Link to='/view-openings?page=1&location=Chennai' className="footer-link">Jobs in Chennai</Link>
+                    <Link to='/view-openings?page=1&location=Pune' className="footer-link">Jobs in Pune</Link>
+                    <Link to='/view-openings?page=1&location=Mumbai' className="footer-link">Jobs in Mumbai</Link>
+                    <Link to='/view-openings?page=1&location=Hyderabad' className="footer-link">Jobs in Hyderabad</Link>
+                    <Link to='/view-openings?page=1&location=New Delhi' className="footer-link">Jobs in New Delhi</Link>
+                    <Link to='/view-openings?page=1&location=Noida' className="footer-link">Jobs in Noida</Link>
+                    <Link to='/view-openings?page=1&location=Gurugram' className="footer-link">Jobs in Gurugram</Link>
                 </div>
             </div>
+            <p className="footer-address" style={{textAlign: 'center'}}>© 2024 EarlyJobs | All rights reserved.</p>
         </footer>
     );
 }
