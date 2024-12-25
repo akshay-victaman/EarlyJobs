2ba8611d6ce7c98373ded0949b0166ccaf25b25a
akshay <akshay.komakula@techolution.com>
2024-12-25 23:02:04 +0530

    fixed the hydration issue

--- client/src/components/AccountManagerPage/index.js
+++ client/src/components/AccountManagerPage/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import NavBar from '../NavBar';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 const AccountManagerPage = () => {
-    return (
-        <div className="account-manager-container">
-            {/* <NavBar /> */}
-            <div className='bde-content'>
-                <h1 className='bde-heading'>Welcome to <span className='head-span'>Account Manager</span> Portal</h1>
-                
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="account-manager-container">
+      <div className="bde-content">
+        <h1 className="bde-heading">
+          Welcome to <span className="head-span">Account Manager</span> Portal
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default AccountManagerPage;
--- client/src/components/HiringPartnerReqPage/index.js
+++ client/src/components/HiringPartnerReqPage/index.js
@@ -1,122 +1,159 @@
-import { query, where, collection, getFirestore, getDocs, orderBy, Timestamp } from "firebase/firestore";
+import {
+  query,
+  where,
+  collection,
+  getFirestore,
+  getDocs,
+  orderBy,
+  Timestamp,
+} from "firebase/firestore";
 import app from "../../firebase";
 import { useEffect, useState } from "react";
-import React from 'react';
-import { IoSearchSharp } from 'react-icons/io5'
-import './style.css'
+import React from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import "./style.css";
 import { Link } from "react-router-dom";
 
 const HiringPartnerReqPage = () => {
+  const [hiringPartnerReqList, setHiringPartnerReqList] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-    const [hiringPartnerReqList, setHiringPartnerReqList] = useState([])
-    const [searchInput, setSearchInput] = useState('')
+  useEffect(() => {
+    const getHiringPartnerReqList = async () => {
+      const db = getFirestore(app);
+      const queryRef = query(
+        collection(db, "HiringPartnerRequests"),
+        where("formData.isApproved", "==", false),
+        orderBy("formData.AppliedDate", "desc")
+      );
 
-    useEffect(() => {
-        const getHiringPartnerReqList = async () => {
-            const db = getFirestore(app);
-            const queryRef = query(
-                collection(db, "HiringPartnerRequests"),
-                where("formData.isApproved", "==", false),
-                orderBy("formData.AppliedDate", "desc"),
+      const querySnap = await getDocs(queryRef);
+
+      if (!querySnap.empty) {
+        const documents = querySnap.docs.map((doc) => {
+          const timestamp = doc.data().formData.AppliedDate;
+
+          // Check if AppliedDate is a Timestamp object:
+          if (!(timestamp instanceof Timestamp)) {
+            console.error(
+              "AppliedDate is not a valid Timestamp. Skipping conversion."
             );
+            return doc.data();
+          }
 
-            const querySnap = await getDocs(queryRef);
+          // Convert Timestamp to human-readable format:
+          const options = {
+            // Configure formatting options as needed
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true, // Use appropriate time format
+            timeZone: "Asia/Kolkata", // Replace with your preferred time zone
+          };
+          const formattedDate = timestamp
+            .toDate()
+            .toLocaleString("en-US", options);
 
-            if (!querySnap.empty) {
-                const documents = querySnap.docs.map((doc) => {
-                    const timestamp = doc.data().formData.AppliedDate;
-                  
-                    // Check if AppliedDate is a Timestamp object:
-                    if (!(timestamp instanceof Timestamp)) {
-                      console.error("AppliedDate is not a valid Timestamp. Skipping conversion.");
-                      return doc.data();
-                    }
-                  
-                    // Convert Timestamp to human-readable format:
-                    const options = { // Configure formatting options as needed
-                      day: '2-digit',
-                      month: 'long',
-                      year: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                      hour12: true, // Use appropriate time format
-                      timeZone: 'Asia/Kolkata' // Replace with your preferred time zone
-                    };
-                    const formattedDate = timestamp.toDate().toLocaleString('en-US', options);
-                  
-                    return {
-                      // Original document data
-                      ...doc.data(),
-                      // Add `formattedDate` property with converted string
-                      formattedDate
-                    };
-                  });
-                console.log(documents)
-                setHiringPartnerReqList(documents)
-            } else {
-                console.log("No such documents!");
-            }
-        }
-        getHiringPartnerReqList()
-    }, [])
+          return {
+            // Original document data
+            ...doc.data(),
+            // Add `formattedDate` property with converted string
+            formattedDate,
+          };
+        });
+        console.log(documents);
+        setHiringPartnerReqList(documents);
+      } else {
+        console.log("No such documents!");
+      }
+    };
+    getHiringPartnerReqList();
+  }, []);
 
-    const handleChangeSearchInput = (e) => {
-        setSearchInput(e.target.value)
-    }
+  const handleChangeSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
 
-    const filteredHrList = hiringPartnerReqList.filter(eachItem => 
-        eachItem.formData.personalDetails.fullName.toLowerCase().includes(searchInput.toLowerCase()) || 
-        eachItem.formData.personalDetails.email.toLowerCase().includes(searchInput.toLowerCase()) ||
-        eachItem.formData.personalDetails.phone.toLowerCase().includes(searchInput.toLowerCase())
-    );
+  const filteredHrList = hiringPartnerReqList.filter(
+    (eachItem) =>
+      eachItem.formData.personalDetails.fullName
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()) ||
+      eachItem.formData.personalDetails.email
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()) ||
+      eachItem.formData.personalDetails.phone
+        .toLowerCase()
+        .includes(searchInput.toLowerCase())
+  );
 
-    return (
-        <div className='homepage-container'>
-            {/* <NavBar /> */}
-            <div className='hiring-partner-req-page-content-con'>
-                <h1 className='hiring-partner-req-heading'>Recruiter Requests</h1>
-                <div className="user-view-search-con" style={{marginBottom: "20px"}}>
-                    <div className="user-view-search-button">
-                        <IoSearchSharp className="search-icon" />
-                    </div>
-                    <input className="user-view-search-input" type="search" value={searchInput} onChange={handleChangeSearchInput} placeholder="Search by name, email, or phone" />
-                </div>
-                <div className='hiring-partner-req-page-content'>
-                    <table className='users-table hiring-partner-req-table'>
-                        <thead>
-                            <tr className='users-table-heading-row'>
-                                <th className="users-table-heading">Name</th>
-                                <th className="users-table-heading">Email</th>
-                                <th className="users-table-heading">Contact</th>
-                                <th className="users-table-heading">Applied Date</th>
-                                <th className="users-table-heading">More Details</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                filteredHrList.map((hiringPartnerReq) => {
-                                    return (
-                                        <tr className="users-table-data-row" key={hiringPartnerReq.formData.docId}>
-                                            <td data-cell='Name' className="users-table-data">{hiringPartnerReq.formData.personalDetails.fullName}</td>
-                                            <td data-cell='Email' className="users-table-data">{hiringPartnerReq.formData.personalDetails.email}</td>
-                                            <td data-cell='Phone' className="users-table-data">{hiringPartnerReq.formData.personalDetails.phone}</td>
-                                            <td data-cell='Applied Date' className="users-table-data">{hiringPartnerReq.formattedDate}</td>
-                                            <td data-cell='More Details' className="users-table-data">
-                                                <Link to={`/admin/recruiter-requests/${hiringPartnerReq.formData.docId}`} className='link'>
-                                                    <button className='hiring-partner-req-btn hrp-button'>View</button>
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            {/* <Footer /> */}
+  return (
+    <div className="homepage-container">
+      <div className="hiring-partner-req-page-content-con">
+        <h1 className="hiring-partner-req-heading">Recruiter Requests</h1>
+        <div className="user-view-search-con" style={{ marginBottom: "20px" }}>
+          <div className="user-view-search-button">
+            <IoSearchSharp className="search-icon" />
+          </div>
+          <input
+            className="user-view-search-input"
+            type="search"
+            value={searchInput}
+            onChange={handleChangeSearchInput}
+            placeholder="Search by name, email, or phone"
+          />
         </div>
-    )
-}
+        <div className="hiring-partner-req-page-content">
+          <table className="users-table hiring-partner-req-table">
+            <thead>
+              <tr className="users-table-heading-row">
+                <th className="users-table-heading">Name</th>
+                <th className="users-table-heading">Email</th>
+                <th className="users-table-heading">Contact</th>
+                <th className="users-table-heading">Applied Date</th>
+                <th className="users-table-heading">More Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredHrList.map((hiringPartnerReq) => {
+                return (
+                  <tr
+                    className="users-table-data-row"
+                    key={hiringPartnerReq.formData.docId}
+                  >
+                    <td data-cell="Name" className="users-table-data">
+                      {hiringPartnerReq.formData.personalDetails.fullName}
+                    </td>
+                    <td data-cell="Email" className="users-table-data">
+                      {hiringPartnerReq.formData.personalDetails.email}
+                    </td>
+                    <td data-cell="Phone" className="users-table-data">
+                      {hiringPartnerReq.formData.personalDetails.phone}
+                    </td>
+                    <td data-cell="Applied Date" className="users-table-data">
+                      {hiringPartnerReq.formattedDate}
+                    </td>
+                    <td data-cell="More Details" className="users-table-data">
+                      <Link
+                        to={`/admin/recruiter-requests/${hiringPartnerReq.formData.docId}`}
+                        className="link"
+                      >
+                        <button className="hiring-partner-req-btn hrp-button">
+                          View
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HiringPartnerReqPage
+export default HiringPartnerReqPage;
