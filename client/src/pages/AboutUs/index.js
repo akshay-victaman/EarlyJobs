35f8ba0b752d98e4fd1e3cc8bd84a05576c08b63
axxxay <akkiakshay440@gmail.com>
2025-02-09 18:06:05 +0530

    updated about page, developed management page

--- client/src/pages/AboutUs/index.js
+++ client/src/pages/AboutUs/index.js
@@ -5,7 +5,7 @@ import { metaConstants } from '../../utils/metaConstants';
 
 const AboutUs = () => {
 
-    const curlturalFit = [
+    const culturalFit = [
         {
             heading1: 'Deep understanding of organizational culture:',
             para1: 'We work closely with our clients to gain a comprehensive understanding of their organizational culture. By having in-depth discussions, conducting site visits, and studying their values, mission, and work environment, we develop a clear picture of the organization’s culture.',
@@ -66,16 +66,55 @@ const AboutUs = () => {
 
             <div className="privacy-policy-page__content">
                 <div className='privacy-policy-content-sub'>
-                    <h3 className='privacy-policy-subheading'>EarlyJobs, is an innovative venture spearheaded by Victaman Services Pvt. Ltd., offers a unique platform tailored for freelance recruiters to operate from anywhere, harnessing the power of remote work. Moreover, it doubles up as a valuable resource hub catering to students enrolled in degree or MBA programs, providing them with extensive training opportunities and avenues for internships.</h3>
+                    <h3 className='privacy-policy-subheading'>EarlyJobs is an innovative platform designed to provide freelance and aspiring recruiters the flexibility to work from anywhere, leveraging the benefits of remote work.                    </h3>
                 </div>
 
                 <div className='privacy-policy-content-sub'>
-                    <h3 className='privacy-policy-subheading'>EarlyJobs extends its benefits to three distinct groups:</h3>
+                    <h3 className='privacy-policy-subheading'>EarlyJobs extends its benefits to five distinct groups:</h3>
                     <ol className='privacy-policy-list'>
-                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>1. </span> Existing recruiters employed in companies can leverage their free time by tapping into freelance opportunities on the platform.</li>
-                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>2. </span> Individuals seeking remote work as recruiters can find ample opportunities to work from the comfort of their homes.</li>
-                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>3. </span> Aspiring professionals eager to gain hands-on experience in recruitment can explore internship options tailored to their career aspirations on EarlyJobs.</li>
+                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>1. </span> Individuals seeking remote work as recruiters can find ample scope to work from the comfort of their homes.</li>
+                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>2. </span> Aspiring professionals eager to gain hands-on experience in recruitment can explore internship options tailored to their career aspirations on Earlyjobs.</li>
+                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>3. </span> Recruiters employed in companies can leverage their spare time by tapping into freelance opportunities on this platform.</li>
+                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>4. </span> This platform offers opportunities to college/university for in-campus Internship in recruitment.</li>
+                        <li className='privacy-policy-item'><span className='privacy-policy-item-span'>5. </span> It also provides franchise opportunities to people who want to begin/set up  their recruitment agency in any part of India</li>
                     </ol>
+                </div>
+
+                {/* <div className='about-us-curtural-fit-container'>
+                    <h1 className='about-us-curtural-fit-heading'>Cultural Fit Between Candidates and Organizations</h1>
+                    <hr className='about-us-curtural-fit-hr' />
+                    <ul className='about-us-curtural-fit-list'>
+                        {culturalFit.map((item, index) => (
+                        <li key={index} className='about-us-curtural-fit-item'>
+                            {item.heading1 && <h3 className='about-us-curtural-fit-subheading'>{item.heading1}</h3>}
+                            {item.para1 && <p className='about-us-curtural-fit-para'>{item.para1}</p>}
+                            <img src={item.img} draggable={false} alt="Cultural Fit Between Candidates and Organizations" className='about-us-curtural-fit-img' />
+                            <h3 className='about-us-curtural-fit-subheading'>{item.heading2}</h3>
+                            <p className='about-us-curtural-fit-para'>{item.para2}</p>
+                        </li>
+                        ))}
+                    </ul>
+                </div> */}
+
+                <div className='privacy-policy-content-sub'>
+                    <h3 className='privacy-policy-subheading' style={{marginTop: '15px'}}>The Story behind Earlyjobs</h3>
+                    <div className='privacy-policy-founder'>
+                        
+                        <ul className='privacy-policy-list founder-content'>
+                            <li className='privacy-policy-item'>For years, <span className='strong-text'>Mr. Ravi</span> had been a prominent figure in the recruitment industry, running his agency with dedication and precision. Yet, amidst the hustle and bustle of corporate corridors, he couldn't shake off the persistent thought of those left behind – the countless youths scattered across the country, yearning for opportunities in the field of recruitment.</li>
+                            <li className='privacy-policy-item'>Their voices echoed in his mind as he pondered over the glaring gap between talent and opportunity. Mr. Ravi couldn't ignore the wave of students clamoring for internships as well as the potential recruiters yearning for remote work  opportunities.</li>
+                            <li className='privacy-policy-item'>Fueled by empathy and driven by a sense of responsibility, Mr. Ravi embarked on a journey of innovation. He envisioned a solution that would not only bridge the chasm between aspiration and achievement but would also redefine the very essence of recruitment itself. Thus, <span className='strong-text'>Earlyjobs was conceived – a portal where dreams intersected with possibilities.</span></li>
+                            <li className='privacy-policy-item'>The platform became a beacon of hope for experienced recruiters as well as countless youths across the nation, offering them the chance to step into the world of recruitment irrespective of their geographical location.</li>
+                            <li className='privacy-policy-item'>In short, EarlyJobs is <span className='strong-text-gray'>revolutionizing recruitment with flexible remote work solutions and nurturing the next generation through internships.</span></li>
+                            <li className='privacy-policy-item'>At Earlyjobs we empower women recruiters as well as aspiring recruiters to utilise their versatility and work from home regardless of the geographical boundaries. <span className='strong-text-gray'>Earlyjobs has a track of an impressive clientele and is already creating ripples in the Indian job sector</span> <span className='strong-text'>Earlyjobs isn't just a portal – it's a testament to the power of empathy, innovation, and the relentless pursuit of a better tomorrow.</span></li>
+                        </ul>
+                        <div className='privacy-policy-image-con'>
+                            <img src="/founder-image.jpg" alt="EarlyJobs Founder Ravi" className='privacy-policy-founder-image' />
+                            <p className='privacy-policy-founder-name'>Mr. Ravi Prakash Kumar</p>
+                            <p className='privacy-policy-founder-title'>Founder, EarlyJobs</p>
+                            <a href="https://www.linkedin.com/in/raviprakashkumar/" target="_blank" rel="noreferrer" className='privacy-policy-founder-linkedin'><FaLinkedin className='linkedin-icon' /></a>
+                        </div>
+                    </div>
                 </div>
 
                 <div className='privacy-policy-content-sub'>
@@ -90,43 +129,6 @@ const AboutUs = () => {
                     <ul className='privacy-policy-list'>
                         <li className='privacy-policy-item'>Our vision at EarlyJobs is to be the foremost global platform, seamlessly connecting talent with opportunities. We envision a future where recruitment is agile, remote work is prevalent, and talent development is prioritized. Through innovation and inclusivity, we aspire to redefine the dynamics of the professional world.</li>
                     </ul>
-                </div>
-
-                <div className='about-us-curtural-fit-container'>
-                    <h1 className='about-us-curtural-fit-heading'>Cultural Fit Between Candidates and Organizations</h1>
-                    <hr className='about-us-curtural-fit-hr' />
-                    <ul className='about-us-curtural-fit-list'>
-                        {curlturalFit.map((item, index) => (
-                        <li key={index} className='about-us-curtural-fit-item'>
-                            {item.heading1 && <h3 className='about-us-curtural-fit-subheading'>{item.heading1}</h3>}
-                            {item.para1 && <p className='about-us-curtural-fit-para'>{item.para1}</p>}
-                            <img src={item.img} draggable={false} alt="Cultural Fit Between Candidates and Organizations" className='about-us-curtural-fit-img' />
-                            <h3 className='about-us-curtural-fit-subheading'>{item.heading2}</h3>
-                            <p className='about-us-curtural-fit-para'>{item.para2}</p>
-                        </li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div className='privacy-policy-content-sub'>
-                    <h3 className='privacy-policy-subheading'>The Story behind Earlyjobs</h3>
-                    <div className='privacy-policy-founder'>
-                        
-                        <ul className='privacy-policy-list founder-content'>
-                            <li className='privacy-policy-item'>For years, <span className='strong-text'>Mr. Ravi</span> had been a stalwart in the recruitment industry, running his agency with dedication and precision. Yet, amidst the hustle and bustle of corporate corridors, he couldn't shake off the persistent thought of those left behind – the countless youths scattered across the country, yearning for opportunities in the field of recruitment.</li>
-                            <li className='privacy-policy-item'>Their voices echoed in his mind as he pondered over the glaring gap between talent and opportunity. Mr. Ravi witnessed the fervor in these young souls, eager to carve their paths in the world of recruitment, but lacking the avenues to do so. Simultaneously, he couldn't ignore the wave of students clamoring for internships, their hunger for real-world experience palpable.</li>
-                            <li className='privacy-policy-item'>Fueled by empathy and driven by a sense of responsibility, Mr. Ravi embarked on a journey of innovation. He envisioned a solution that would not only bridge the chasm between aspiration and achievement but would also redefine the very essence of recruitment itself. Thus, <span className='strong-text'>EarlyJobs was conceived – a portal where dreams intersected with possibilities.</span></li>
-                            <li className='privacy-policy-item'>With each line of code and every strategic decision, Mr. Ravi infused EarlyJobs with his unwavering commitment to empowerment. The platform became a beacon of hope for countless youths across the nation, offering them the chance to step into the realm of recruitment as freelance recruiters, irrespective of their geographical location.</li>
-                            <li className='privacy-policy-item'>Simultaneously, EarlyJobs opened its doors to students hungry for knowledge and experience, providing them with internships that transcended boundaries and limitations. Remote work became the norm, and flexibility became synonymous with success.</li>
-                            <li className='privacy-policy-item'>Today, as the sun rises on EarlyJobs, its impact reverberates far and wide. Over a hundred youths have found solace and opportunity within its virtual corridors, carving their paths as freelance recruiters or diving headfirst into transformative internships. And as Mr. Ravi looks upon the fruits of his labor, <span className='strong-text'>he knows that EarlyJobs isn't just a portal – it's a testament to the power of empathy, innovation, and the relentless pursuit of a better tomorrow.</span></li>
-                        </ul>
-                        <div className='privacy-policy-image-con'>
-                            <img src="/founder-image.jpg" alt="EarlyJobs Founder Ravi" className='privacy-policy-founder-image' />
-                            <p className='privacy-policy-founder-name'>Mr. Ravi Prakash Kumar</p>
-                            <p className='privacy-policy-founder-title'>Founder, EarlyJobs</p>
-                            <a href="https://www.linkedin.com/in/raviprakashkumar/" target="_blank" rel="noreferrer" className='privacy-policy-founder-linkedin'><FaLinkedin className='linkedin-icon' /></a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
---
+++ client/src/pages/ManagementPage/index.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect } from 'react'
+import { metaConstants } from '../../utils/metaConstants';
+import './style.css'
+
+const ManagementPage = () => {
+
+    const managementList = [
+        {
+            name: 'Asish Chakraborty',
+            designation: 'CEO & Co-Founder',
+            desc: 'Asish Chakraborty is the CEO and Co-Founder of Earlyjobs, a company dedicated to transforming the recruitment process by fostering meaningful connections that fuel growth for both employers and jobseekers. He holds an MBA from the Sikkim Manipal Institute of Technology and brings with him 20 years of extensive experience in the pharmaceutical industry. Asish is passionate about introducing changes to the employment sector by promoting freelancers and empowering women to work from home. His expertise and leadership skills serve as a guiding force in propelling Earlyjobs to greater heights.',
+            img: '/about_us_imgs/Asish-Chakraborty.jpg'
+        },
+        {
+            name: 'Ravi Kumar',
+            designation: 'Founder',
+            desc: "Ravi Kumar originating from Gaya in Bihar, the Founder of Earlyjobs has been a stalwart in the recruitment industry running his agency with dedication and precision. He as an overall experience of 10+ years of experience in various domains. With over a decade of experience across various domains, he has been instrumental in overseeing all operational aspects of Victaman, ensuring efficiency, growth, and strategic alignment. Ravi Prakash Kumar's leadership has been pivotal in driving the success and growth of both EarlyJobs and VictaMan Services Pvt. Ltd., contributing significantly to the recruitment industry.",
+            img: '/about_us_imgs/Ravi-Prakash.jpg'
+        },
+        {
+            name: 'Saurav Kumar',
+            designation: 'Co-Founder & Strategic Adviser ',
+            desc: "Saurav Kumar is a dynamic business strategist and entrepreneur with over 10 years of experience in driving business growth, innovation, and operational excellence. He holds a Master’s degree from SRM University, Chennai, and has founded multiple successful ventures, including Victaman Services Pvt Ltd, Goformeet, English Wizard, and Meet XO. <br /> <br /> Currently, Saurav plays a key role in planning, execution, and operations at Earlyjobs, leveraging his expertise in IT solutions, business development, and strategic leadership.He envisions revolutionizing the job market in the country by providing employment opportunities to the masses, reflecting his commitment to impactful and scalable solutions.",
+            img: '/about_us_imgs/Saurav-Kumar.jpg'
+        },
+        {
+            name: 'Surbhi Rani',
+            designation: 'Co-Founder',
+            desc: "Surbhi Rani, a graduate of Magadh University, is a dynamic leader specializing in building diverse, high-performing teams and crafting impactful recruitment strategies. As the Director of EarlyJobs, she plays a pivotal role in full-cycle recruitment, client relationship management, and ensuring a seamless candidate experience. With a deep understanding of talent acquisition, Surbhi is committed to connecting top talent with the right opportunities, driving growth for both candidates and organizations.",
+            img: '/about_us_imgs/Surbhi-Rani.jpg'
+        },
+        {
+            name: 'Akanksha',
+            designation: 'Operations',
+            desc: "Akanksha is a Senior Hiring Manager at Earlyjobs, bringing over a year of experience in recruitment and leadership. A graduate of the University of Calcutta, she is passionate about hiring new talent for organizations. In her role, Akansha is responsible for developing and implementing effective recruitment strategies, sourcing and attracting qualified candidates, and collaborating with hiring managers to understand their staffing needs. Her dedication to talent acquisition and her leadership skills contribute significantly to the growth and success of Earlyjobs.",
+            img: '/about_us_imgs/Akanksha.jpg'
+        },
+    ]
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+        document.title = metaConstants.management.title
+
+        const metaDescription = document.querySelector('meta[name="description"]');
+        const metaKeywords = document.querySelector('meta[name="keywords"]');
+        const metaSubject = document.querySelector('meta[name="subject"]');
+        if (metaDescription) {
+            metaDescription.setAttribute('content', metaConstants.about.description);
+        }
+        if (metaKeywords) {
+            metaKeywords.setAttribute('content', metaConstants.about.keywords);
+        }
+        if (metaSubject) {
+            metaSubject.setAttribute('content', metaConstants.about.description);
+        }
+
+        return () => {
+            document.title = metaConstants.title
+            if (metaDescription) {
+                metaDescription.setAttribute('content', metaConstants.description); // Replace with the original content if needed
+            }
+            if (metaKeywords) {
+                metaKeywords.setAttribute('content', metaConstants.keywords);
+            }
+            if (metaSubject) {
+                metaSubject.setAttribute('content', metaConstants.description);
+            }
+        };
+    }, [])
+
+    return (
+        <div className="privacy-policy-page">
+            <div className="privacy-policy-page__background">
+                <h1 className='privacy-policy-heading'>Management Team</h1>
+            </div>
+
+            <div className="management-page__content">
+                <div className="management-page__content__team">
+                    <h2 className="management-page__content__team__heading" >Management Team</h2>
+                    <p className="management-page__content__team__para">Our management team is responsible for the day-to-day operations of our company. They are committed to providing the highest level of service to our customers and ensuring that our business operates efficiently and effectively.</p>
+                </div>
+                <img src="/about_us_imgs/management-img.webp" alt="management team" className="management-page__content__image" />
+            </div>
+
+            { managementList.map((item, index) => (
+                <div className='management-team-con'>
+                    {index%2 === 0 && <img src={item.img} alt="management team" className="management-team-img" /> }
+                    <div className='management-team-detail'>
+                        <h1 className='management-team-name'>{item.name}</h1>
+                        <p className='management-team-designation'>{item.designation}</p>
+                        <p className='management-team-desc'>{item.desc}</p>
+                    </div>
+                    {index%2 !== 0 && <img src={item.img} alt="management team" className="management-team-img" /> }
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default ManagementPage
