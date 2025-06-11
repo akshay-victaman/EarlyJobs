a2f1d263bdb5b7d5bddde298dea365e36ff316f7
Yaswanthram-396 <yaswanthram.396@gmail.com>
2025-06-11 19:36:12 +0530

    fix: update server port to 5000

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use('/api/recommendations', recommendationRoutes);
 app.use("/", blogRouter);
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
  app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
